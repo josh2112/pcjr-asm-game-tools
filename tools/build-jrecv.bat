7f11af92d8d1adb321573c530235a122b166047b
Joshua Foster <josh2112@gmail.com>
2025-03-10 18:04:04 -0400

    fixed bug in jrecv.cpp...?

--- tools/build-jrecv.bat
+++ tools/build-jrecv.bat
@@ -1,2 +1,2 @@
 
-EmuCR-Dosbox-r4059\dosbox.exe -conf WATCOM/watcom.conf -c "CD MTCP\APPS\JRECV" -c WMAKE
+D:\Repos\EmuJr\Tools\dosbox-staging-windows-x64-v0.81.0-alpha-2007-g4cc1c1\dosbox.exe -conf WATCOM/watcom.conf -c "CD MTCP\APPS\JRECV" -c WMAKE
