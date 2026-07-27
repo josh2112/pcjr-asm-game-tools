edecb18e01e1e29383c16765de6422f764d02242
Joshua Foster <josh2112@gmail.com>
2026-07-27 13:13:54 -0400

    Rewrote makeimg.sh and extractimg.sh to /bin/sh for Alpine Linux

--- floppyimages/extractimg.sh
+++ floppyimages/extractimg.sh
@@ -1,70 +1,93 @@
-#!/bin/bash
-# makeimg.sh: Makes a 360 KB floppy image containing the contents of a particular directory, suitable for being
+#!/bin/sh
+# extractimg.sh: Extracts a floppy image containing the contents of a particular directory, suitable for being
 # served by mTCP's netserve.
 #
-# Usage: ./extractimg.sh [-o|--output destdir] imgpath
-#     imgpath  the image file to extract
-#     destdir  the destination directory (default: imgpath with extension removed )
+# Usage: ./extractimg.sh [-f|--force] [-o|--output destdir] imgpath
+#   imgpath the image file to extract
+#   destdir the destination directory (default: imgpath with extension removed)
 
-set -o errexit -o pipefail -o noclobber -o nounset
+set -e  # errexit
+set -C  # noclobber
+set -u  # nounset
 
-FORCE=n OUTPUT=-
+FORCE=n
+OUTPUT=-
 
 PARSED=$(getopt --options='fo:' --longoptions='force,output:' --name "$0" -- "$@") || exit 2
 eval set -- "$PARSED"
 
 while true; do
-    case "$1" in
-        -f|--force)
-            FORCE=y
-            shift
-            ;;
-        -o|--output)
-            OUTPUT="$2"
-            shift 2
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Programming error"
-            exit 2
-            ;;
-    esac
+  case "$1" in
+    -f|--force)
+      FORCE=y
+      shift
+      ;;
+    -o|--output)
+      OUTPUT="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Programming error"
+      exit 2
+      ;;
+  esac
 done
 
-if [[ $# -ne 1 ]]; then
-    echo "$0: No disk image name given!"
-    exit 1
+if [ "$#" -ne 1 ]; then
+  echo "$0: No disk image name given!"
+  exit 1
 fi
 
-IMGPATH="${1//\\//}"
+IMGPATH=$(echo "$1" | tr '\\' '/')
 IMGNAME="${IMGPATH##*/}"
 
-if [[ "$OUTPUT" == "-" ]]; then
-    OUTPUT="${IMGPATH%.img}"
+# Use the image name without extension if no output file given
+if [ "$OUTPUT" = "-" ]; then
+  # Strip the .img extension using POSIX suffix removal
+  OUTPUT="${IMGPATH%.img}"
 fi
 
+cleanup() {
+  EXIT_STATUS=$?
+  
+  echo "Cleaning up..."
 
-if [[ -e "$OUTPUT" ]]; then
-    if [[ "$FORCE" == 'y' ]]; then
-        rm -r "$OUTPUT"
-    else
-        echo "Output directory already exists. If you want to overwrite it, use -f."
-        exit 1
-    fi
+  if mount | grep -q "on /mnt/$IMGNAME "; then
+    sudo umount "/mnt/$IMGNAME"
+  fi
+  
+  if [ -d "/mnt/$IMGNAME" ]; then
+    sudo rm -rf "/mnt/$IMGNAME"
+  fi
+
+  exit $EXIT_STATUS
+}
+
+trap cleanup EXIT INT TERM
+
+# Handle existing destination directory
+if [ -e "$OUTPUT" ]; then
+  if [ "$FORCE" = 'y' ]; then
+    # Temporarily bypass -C (noclobber) to allow removal
+    set +C
+    rm -r "$OUTPUT"
+    set -C
+  else
+    echo "Output directory already exists. If you want to overwrite it, use -f."
+    exit 1
+  fi
 fi
 
 mkdir -p "$OUTPUT"
-
 sudo mkdir -p "/mnt/$IMGNAME"
 
-sudo mount $IMGPATH "/mnt/$IMGNAME"
+# Mount the target floppy image
+sudo mount "$IMGPATH" "/mnt/$IMGNAME"
 echo "Mounted $IMGPATH at /mnt/$IMGNAME"
 
 echo "Copying contents of image to $OUTPUT..."
-sudo cp -vr "/mnt/$IMGNAME"/* "$OUTPUT"
-
-sudo umount "/mnt/$IMGNAME"
-echo "Unmounted $IMGPATH"
+sudo cp -r "/mnt/$IMGNAME"/* "$OUTPUT"
--- floppyimages/makeimg.sh
+++ floppyimages/makeimg.sh
@@ -1,71 +1,92 @@
-#!/bin/bash
+#!/bin/sh
 # makeimg.sh: Makes a 360 KB floppy image containing the contents of a particular directory, suitable for being
 # served by mTCP's netserve.
 #
 # Usage: ./makeimg.sh [-f] [-o|--output imgpath] filespath
-#     filespath  the directory containing the contents of the image
-#     imgpath    the image file to create (default: filespath.img)
+#   filespath the directory containing the contents of the image
+#   imgpath the image file to create (default: filespath.img)
 
-set -o errexit -o pipefail -o noclobber -o nounset
+set -e  # errexit
+set -C  # noclobber
+set -u  # nounset
 
-
-FORCE=n OUTPUT=-
+FORCE=n
+OUTPUT=-
 
 PARSED=$(getopt --options='fo:' --longoptions='force,output' --name "$0" -- "$@") || exit 2
 eval set -- "$PARSED"
 
 while true; do
-    case "$1" in
-        -f|--force)
-            FORCE=y
-            shift
-            ;;
-        -o|--output)
-            OUTPUT="$2"
-            shift 2
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Programming error"
-            exit 2
-            ;;
-    esac
+  case "$1" in
+    -f|--force)
+      FORCE=y
+      shift
+      ;;
+    -o|--output)
+      OUTPUT="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Programming error"
+      exit 2
+      ;;
+  esac
 done
 
-if [[ $# -ne 1 ]]; then
-    echo "$0: No image directory given!"
-    exit 1
+if [ "$#" -ne 1 ]; then
+  echo "$0: No image directory given!"
+  exit 1
 fi
 
-FILESPATH="${1//\\//}"
+FILESPATH=$(echo "$1" | tr '\\' '/')
 
-if [[ "$OUTPUT" == "-" ]]; then
-    OUTPUT="${FILESPATH%/}.img"
+# Use [filespath].img if no output file given
+if [ "$OUTPUT" = "-" ]; then
+  CLEAN_PATH="${FILESPATH%/}"
+  OUTPUT="${CLEAN_PATH}.img"
 fi
 
 IMGNAME="${OUTPUT##*/}"
 
-if [[ -e "$OUTPUT" ]]; then
-    if [[ "$FORCE" == 'y' ]]; then
-        rm "$OUTPUT"
-    else
-        echo "Image file already exists. If you want to overwrite it, use -f."
-        exit 1
-    fi
+if [ -e "$OUTPUT" ]; then
+  if [ "$FORCE" = 'y' ]; then
+    # Bypass -C (noclobber) restrictions when forcefully replacing an existing file
+    set +C
+    rm "$OUTPUT"
+    set -C
+  else
+    echo "Image file already exists. If you want to overwrite it, use -f."
+    exit 1
+  fi
 fi
 
-dd if=/dev/zero of="$OUTPUT" bs=1K count=360
+cleanup() {
+  EXIT_STATUS=$?
+  
+  echo "Cleaning up..."
+  
+  if mount | grep -q "on /mnt/$IMGNAME "; then
+    sudo umount "/mnt/$IMGNAME"
+  fi
+  
+  if [ -d "/mnt/$IMGNAME" ]; then
+    sudo rm -rf "/mnt/$IMGNAME"
+  fi
+
+  exit $EXIT_STATUS
+}
+
+trap cleanup EXIT INT TERM
+
+dd if=/dev/zero of="$OUTPUT" bs=1k count=360
 mkfs.fat -F12 "$OUTPUT"
 
 sudo mkdir -p "/mnt/$IMGNAME"
-
 sudo mount "$OUTPUT" "/mnt/$IMGNAME"
-
-sudo cp -vr "$FILESPATH"/* "/mnt/$IMGNAME"
-
-sudo umount "/mnt/$IMGNAME"
+sudo cp -r "$FILESPATH"/* "/mnt/$IMGNAME"
 
 echo "Created $OUTPUT"
